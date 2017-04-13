31e7e0094d38ffec8b2bcb5aaf1425ae28c2a8a0
Vincent Bouvier <vincent.bouvier@ismart.fr>
2017-04-13 11:06:40 +0200

    Change version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='mstranslator',
-    version='0.3.2',
+    version='0.3.3',
     license='MIT',
     url='https://github.com/wronglink/mstranslator',
     bugtrack_url='https://github.com/wronglink/mstranslator/issues',
